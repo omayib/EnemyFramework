151028206c23016bcbf27a11d08ee18c942fe473
arif akbarul huda <omayib@gmail.com>
2017-03-05 08:16:37 +0700

    Initial commit

---
+++ Example/EnemyFramework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EnemyFramework
+//
+//  Created by arief.themil@gmail.com on 03/05/2017.
+//  Copyright (c) 2017 arief.themil@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
